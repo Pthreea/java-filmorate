7c14b934eeba6bca4680e22ce01333f29a32c419
Pthreea <lukas6289@outlook.com>
2026-03-29 13:33:57 +0400

    import

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -11,8 +11,6 @@ import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
 
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
 
 @Data
 public class User {
